e811864f852f438f39cac20e447918fb52ed75c9
Guilherme Parente <guilherme.parente17@outlook.com>
2020-10-24 19:18:32 -0300

    Styling NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      teste
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
